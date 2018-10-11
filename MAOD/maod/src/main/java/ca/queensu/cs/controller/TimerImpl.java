40ad29c11e8b82f4dc6b89fabac574b395cbe068
Majid Babaei <babaee.majid@gmail.com>
2018-10-11 16:51:06 -0400

    adding jar file to achieve time in nanoSec

---
+++ MAOD/maod/src/main/java/ca/queensu/cs/controller/TimerImpl.java
@@ -0,0 +1,11 @@
+package ca.queensu.cs.controller;
+
+import org.eclipse.microprofile.metrics.Timer;
+
+import java.time.*;
+
+
+public class TimerImpl implements Timer {
+	private final long offset;
+
+}
